640b37828e5577afc17163ab5258a5095914d182
Lúcio Brito <luciopereirabritoo@gmail.com>
2022-10-22 12:38:47 -0300

    api funcional em memoria

---
+++ backend/API/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using API.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("");
+
+builder.Services.AddDbContext<APIDbContext>(o => {
+    o.UseInMemoryDatabase("DesafioAPI");
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Desafio Estágio API",
+        Description = "Uma Web API para um CRUD de cliente",
+        Contact = new OpenApiContact
+        {
+            Name = "Lúcio Brito",
+            Url = new Uri("https://github.com/luciopbrito/desafio_estagio_wesley")
+        }
+    });
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
